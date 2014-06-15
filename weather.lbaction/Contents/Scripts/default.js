5aa75c09ddc0247db11c05a01638a1b76e53a848
Danny Siu <danny.siu@gmail.com>
2014-06-15 23:24:06 +0800

    skeleton for porting alfred weather addon to launchbar

---
+++ weather.lbaction/Contents/Scripts/default.js
@@ -0,0 +1,14 @@
+// The 'run' function is called by LaunchBar when the user opens the action.
+
+function run( argument ) {
+
+  if ( argument != undefined ) {
+//    var musics = new Musics(argument);
+//    return musics.getSuggestions();
+  }
+  else {
+    // No argument passed, just open the website:
+//    return LaunchBar.openURL('http://movie.douban.com/');
+  }
+
+}
